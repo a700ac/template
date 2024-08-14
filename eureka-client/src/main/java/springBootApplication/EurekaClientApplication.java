3c6a4ff53c5caa19c842b9430daea3f7b72040a2
a700ac <a700ac@gmail.com>
2024-08-14 16:27:05 +0300

    1

---
+++ eureka-client/src/main/java/springBootApplication/EurekaClientApplication.java
@@ -0,0 +1,16 @@
+package springBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EurekaClientApplication.class, args);
+
+    }
+
+}
